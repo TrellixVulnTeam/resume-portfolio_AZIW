89d9d1aa615aa0625e079f5fd02e2db912b81594
Chandan Gowda Nandakumar <chandangowdanandakumar@outlook.com>
2021-09-23 17:18:19 -0700

    heroku build

--- server.js
+++ server.js
@@ -1,18 +1,9 @@
-function requireHTTPS(req, res, next) {
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
 const express = require('express');
-const app = express();
+const path = require('path');
 const app = express();
 app.use(requireHTTPS);
-
-app.use(express.static('./dist/resume-portfolio'));
+app.use(express.static(__dirname + '/dist/resume-portfolio'));
 app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/resume-portfolio'}
-  );
+    res.sendFile(path.join(__dirname+'/dist/resume-portfolio/index.html'));
   });
-
-  app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080);
