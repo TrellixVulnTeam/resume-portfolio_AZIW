294cd59b6aae7fb9b1ae4c71062b133ad468cbe5
Chandan Gowda Nandakumar <chandangowdanandakumar@outlook.com>
2021-09-23 17:21:54 -0700

    Heroku Build Error

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(requireHTTPS);
 app.use(express.static(__dirname + '/dist/resume-portfolio'));
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname+'/dist/resume-portfolio/index.html'));
